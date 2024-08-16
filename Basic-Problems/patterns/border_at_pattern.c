2185e00ad5e28759e8b75dc5e73a443364d586c8
Aadesh-Sakhare-Dev <aadesh.sakhare.dev@gmail.com>
2024-08-16 23:27:50 +0530

    Added pattern printing programs

---
+++ Basic-Problems/patterns/border_at_pattern.c
@@ -0,0 +1,87 @@
+/*
+ * ============================================================================
+ * Program: Display a pattern of '*' and '@' based on user input.
+ * Author: Aadesh Dipak Sakhare
+ * Date: 16/08/2024
+ * Description:
+ *   This program accepts the number of rows and columns from the user and
+ *   displays a pattern of '*' and '@' characters. The pattern displays '*' 
+ *   on the borders and '@' in the inner area, except for the border rows 
+ *   and columns.
+ * Example:
+ * Input : iRow = 6    iCol = 5
+ * Output:
+ * *   *   *   *   *
+ * *   @   @   @   *
+ * *   @   @   @   *
+ * *   @   @   @   *
+ * *   *   *   *   *
+ * ============================================================================
+ */
+
+
+#include<stdio.h>
+
+/*
+ * ============================================================================
+ * Function Name : Pattern
+ * Description :   Prints a pattern of '*' and '@' characters based on the
+ *                  number of rows and columns provided by the user. The pattern
+ *                  has '*' on the borders and '@' in the inner area.
+ * Input :         Integer, Integer
+ * Output :        Void
+ * Author:         Aadesh Dipak Sakhare
+ * Date:           16/08/2024
+ * ============================================================================
+ */
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+	// Loop through each row
+	for(i= 1; i <= iRow; i++)	
+	{
+		// Loop through each column in the current row
+		for(j=1; j<=iCol; j++) 
+		{
+			// Loop through each column in the current row
+			if((i == 1) || (j == 1) || (i == iRow) || (j == iCol)) 
+			{
+				printf("*\t");
+			}
+			else
+			{
+				printf("@\t");
+			}
+		}
+		printf("\n");	// Move to the next line after each row is printed
+    }
+}
+
+/*
+* ============================================================================
+*   Function Name :     main
+*   Description :       Its a entry point function.
+* ============================================================================
+*/
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+	// Get input from user for the number of rows
+    printf("Enter number of rows : ");
+    scanf("%d",&iValue1);
+
+	// Get input from user for the number of rows
+    printf("Enter number of column : ");
+    scanf("%d",&iValue2);
+
+	// Call the Pattern function with user input values to print the pattern
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
